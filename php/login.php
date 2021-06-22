985556ba6333b926094375c94e14225692e4fc86
BuildTools <fefeucn_cm@hotmail.com>
2021-06-21 22:25:39 -0300

    Atualização validação de e-mails não encontrados

--- php/login.php
+++ php/login.php
@@ -6,16 +6,21 @@
 
     $pdo = Database::connect();
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "SELECT * FROM USUARIOS WHERE EMAIL LIKE ?";
+    $sql = "SELECT * FROM USUARIOS WHERE EMAIL LIKE ?;";
     $query = $pdo->prepare($sql);
-    $query->execute(array($user));
-    $user = $query->fetch(PDO::FETCH_ASSOC);
+    $query->execute(array($email));
     Database::disconnect();
-
-    if ($dbUser['senha'] == $password) {
-        session_start();
-        $_SESSION['email'] = $email;
-        $_SESSION['nome'] = $user['nome'];
-        header("location: painel.php");
+    
+    if ($query->rowCount() != 0) {
+        $user = $query->fetch(PDO::FETCH_ASSOC);
+        if ($user['senha'] == $password) {
+            session_start();
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['usuario'] = $user['usuario'];
+            header("location: painel.php");
+        }
+    } else {
+        header("location: index.php");
     }
+        
 ?>
