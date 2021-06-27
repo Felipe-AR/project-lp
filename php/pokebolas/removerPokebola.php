896dbe37106028addeaf591d9840dd6b87a32d89
BuildTools <fefeucn_cm@hotmail.com>
2021-06-27 20:48:53 -0300

    Correções validando a pokébola ao realizar a exclusão

--- php/pokebolas/removerPokebola.php
+++ php/pokebolas/removerPokebola.php
@@ -6,14 +6,27 @@
 
     // posteriormente realizar uma validação se já existe um usuário utilizando essa pokébola, antes de realizar a exclusão.
 
+
     if (!empty($id)) {
+
         $pdo = Database::connect();
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "DELETE FROM POKEBOLA WHERE ID = ?";
-        $query = $pdo->prepare($sql);
+        $sqlSelectJogador = "SELECT * FROM JOGADOR INNER JOIN POKEBOLA ON (POKEBOLA.ID = JOGADOR.POKEBOLA) WHERE POKEBOLA.ID = ?";
+        $query = $pdo->prepare($sqlSelectJogador);
         $query->execute(array($id));
-        header("location: configurar.php");
 
+        echo $query->rowCount();
+
+        if($query->rowCount() > 0) {
+            header("location: configurar.php?erro=" . $query->rowCount());
+        } else {
+            $sqlRemovePokebola = "DELETE FROM POKEBOLA WHERE ID = ?";
+            $query = $pdo->prepare($sqlRemovePokebola);
+            $query->execute(array($id));
+            header("location: configurar.php");
+        }
         Database::disconnect();
+    } else {
+        header("location: configurar.php");
     }
 ?>
